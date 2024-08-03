888728e6777d7e80fedb8726b5fed2012325dd8f
tejasgadgil <tejassgadgil@gmail.com>
2024-08-03 16:30:39 +0530

    enemy slimes, better dying, player controls and animations, text, scores, audio. Lvl1 is complete!

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,14 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	print("you died") # Replace with function body.
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	#body.get_node("")
 	timer.start()
 	
 
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1	
 	get_tree().reload_current_scene()
